0bc85c24901a805e98fc7b64a1e89bc9cbba7576
rkiko <rkiko@geomar.de>
2021-09-22 17:12:45 +0200

    conducted flux calculation

--- Scripts/Convert_raw_particle_data_to_pangaea_style_dataset.py
+++ Scripts/Convert_raw_particle_data_to_pangaea_style_dataset.py
@@ -17,19 +17,17 @@ if __name__ == '__main__':
     from pathlib import Path
     path_to_data = Path('~/GIT/AC_Agulhas_eddy_2021/Data').expanduser()
     os.chdir(str(path_to_data))
-    test = 1
+    test = 0
     if test == 1:
         df = pd.read_csv("Ecopart_histogram_data_raw.tsv", sep='\t', nrows = 200) #use nrows for testing
     else:
         df = pd.read_csv("Ecopart_histogram_data_raw.tsv", sep='\t') #use nrows for testing
     df = process_raw_ecopart_histo_df(df)
-    df = calculate_nbss_slope_func(df)
     df = calculate_flux_func(df)
     df = calculate_mip_and_map_abundance_func(df)
-    df = calculate_mip_and_map_biovolume_func(df)
-    df = df[["Cruise", "RAWfilename", "CTD_filename", "Latitude", "Longitude", "Date_Time", "Pressure [dbar]", "Vol [L] (sampled for this depth bin)", "MiP_abun", "MaP_abun", "MiP_biov", "MaP_biov", "NBSS slope", 'NBSS intercept', "NBSS slope stde", "Flux_mgC_m2"]]
+    df = df[["RAWfilename", "Latitude", "Longitude", "Date_Time", "Pressure [dbar]", "Vol [L] (sampled for this depth bin)", "MiP_abun", "MaP_abun", "Flux_mgC_m2"]]
     if test == 1:
-        df.to_csv('Ecopart_mip_map_nbss_flux_data_test.tsv', sep='\t', index=False)
+        df.to_csv('Ecopart_mip_map_flux_data_test.tsv', sep='\t', index=False)
     else:
-        df.to_csv('Ecopart_mip_map_nbss_flux_data.tsv', sep='\t', index=False)
+        df.to_csv('Ecopart_mip_map_flux_data.tsv', sep='\t', index=False)
 
