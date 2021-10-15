c449ebf3b003985acc6f1b8c8a4ce5994f92ee79
rkiko <rkiko@geomar.de>
2021-10-15 17:47:49 +0200

    updated scripts and dataset

--- Scripts/Automate_data_download.py
+++ Scripts/Automate_data_download.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     from pathlib import Path
     path_to_scripts = Path('~/GIT/AC_Agulhas_eddy_2021/Scripts').expanduser()
     os.chdir(str(path_to_scripts))
-    os.system("python Download_particle_histogram.py")
+    #os.system("python Download_particle_histogram.py")
     os.system("python Convert_raw_particle_data_to_pangaea_style_dataset.py")
+    os.system("python Calculate_particle_diagnostics.py")
 
