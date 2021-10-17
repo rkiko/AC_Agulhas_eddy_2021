afc38da09b2c899c2bdf8e4dd898dcb0580651cc
ABaudena <alberto.baudena@gmail.com>
2021-10-17 10:10:07 +0200

    added line to call the script which calls the paruvpy function which append the BGC variables to the data file

--- Scripts/Automate_data_download.py
+++ Scripts/Automate_data_download.py
@@ -8,5 +8,6 @@ if __name__ == '__main__':
     os.chdir(str(path_to_scripts))
     #os.system("python Download_particle_histogram.py")
     os.system("python Convert_raw_particle_data_to_pangaea_style_dataset.py")
+    os.system("python Append_BGC_variables.py")
     os.system("python Calculate_particle_diagnostics.py")
 
