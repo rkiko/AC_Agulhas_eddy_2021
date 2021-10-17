8f76e153884938a5d5745a0d93e7e82e4e0cfcd4
ABaudena <alberto.baudena@gmail.com>
2021-10-17 12:10:41 +0200

    script to download the Coriolis BGC data of a given series of float id

---
+++ Scripts/Download_BGC_variables.py
@@ -0,0 +1,13 @@
+'''
+Download the BGC variables from the Coriolis data center via ftp
+'''
+from paruvpy import download_Coriolis_data
+
+
+if __name__ == '__main__':
+    import os
+    from pathlib import Path
+    path_to_data = Path('~/GIT/AC_Agulhas_eddy_2021/Data').expanduser()
+    os.chdir(str(path_to_data))
+    valid_BGCArgo_id_list=['6903095']
+    download_Coriolis_data(valid_BGCArgo_id_list)
