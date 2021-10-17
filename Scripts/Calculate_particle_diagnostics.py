283fb4f04ffc4f0484ae0aa325bbae19b75dc938
rkiko <rkiko@geomar.de>
2021-10-17 17:14:32 +0200

    added the calculation of particle respiration rates from UVP5 data

--- Scripts/Calculate_particle_diagnostics.py
+++ Scripts/Calculate_particle_diagnostics.py
@@ -6,6 +6,7 @@ import pandas as pd
 from paruvpy import calculate_mip_and_map_abundance_w_header_from_df_func
 from paruvpy import calculate_flux_w_header_from_df_func
 from paruvpy import calculate_mip_and_map_poc_cont_w_header_from_df_func
+from paruvpy import calculate_respi_diffusion_limited_w_header_from_df_func
 
 
 
@@ -21,8 +22,12 @@ if __name__ == '__main__':
     df = calculate_flux_w_header_from_df_func(df)
     df = calculate_mip_and_map_abundance_w_header_from_df_func(df)
     df = calculate_mip_and_map_poc_cont_w_header_from_df_func(df)
+    temperature = df["Temperature [degrees Celsius]"]
+    oxygen = df["Doxy [micromol/kg]"]
 
-    df = df[["RAWfilename", "Latitude", "Longitude", "Date_Time", "Pressure [dbar]", "Vol [L] (sampled for this depth bin)", "MiP_abun", "MaP_abun", "Mip_POC_cont_mgC_m3", "Map_POC_cont_mgC_m3", "Flux_mgC_m2"]]
+    df = calculate_respi_diffusion_limited_w_header_from_df_func(df, temperature, oxygen, 4)
+
+    df = df[["RAWfilename", "Latitude", "Longitude", "Date_Time", "Pressure [dbar]", "Vol [L] (sampled for this depth bin)", "MiP_abun", "MaP_abun", "Mip_POC_cont_mgC_m3", "Map_POC_cont_mgC_m3", "Flux_mgC_m2", "Respi_nmolO2_l_h"]]
 
     df.to_csv('Ecopart_mip_map_flux_data.tsv', sep='\t', index=False)
 
