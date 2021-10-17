31a7e335bc4287cc6708f47c36bd7c1e67727780
ABaudena <alberto.baudena@gmail.com>
2021-10-17 12:11:00 +0200

    updated

--- Scripts/Automate_data_download.py
+++ Scripts/Automate_data_download.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     path_to_scripts = Path('~/GIT/AC_Agulhas_eddy_2021/Scripts').expanduser()
     os.chdir(str(path_to_scripts))
     #os.system("python Download_particle_histogram.py")
+    #os.system("python Download_BGC_variables.py")
     os.system("python Convert_raw_particle_data_to_pangaea_style_dataset.py")
     os.system("python Append_BGC_variables.py")
     os.system("python Calculate_particle_diagnostics.py")
