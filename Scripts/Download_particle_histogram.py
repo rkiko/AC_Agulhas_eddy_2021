30e683f30ed1af15cd7c59d5b3931c9e96ed2dc6
ABaudena <alberto.baudena@gmail.com>
2021-10-19 08:31:48 +0200

    added manual insertion of usr and passwd, and option to skip the function if not necessary to download the data

--- Scripts/Download_particle_histogram.py
+++ Scripts/Download_particle_histogram.py
@@ -6,12 +6,14 @@ import sys
 if __name__ == '__main__':
     import os, sys
     from pathlib import Path
+    import getpass
     path_to_data = Path('~/GIT/AC_Agulhas_eddy_2021/Data').expanduser()
     os.chdir(str(path_to_data))
 
-    user_ = sys.argv[1]
-    passwd = sys.argv[2]
-
+    user_ = getpass.getpass('Username (note, if you want to exit the script Download_particle_histogram, please write "e" as input:')
+    if user_=='e':  print('exited from Download_particle_histogram');exit()
+    passwd = getpass.getpass('Password (note, if you want to exit the script Download_particle_histogram, please write "e" as input::')
+    if passwd == 'e':  print('exited from Download_particle_histogram');exit()
     mydb = db_connect(user_, passwd)
     valid_projectid_list = '(356)'
     df = read_particle_histogram_data_floats(valid_projectid_list, mydb)
