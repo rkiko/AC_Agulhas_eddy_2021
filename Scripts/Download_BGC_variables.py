a10a5af1d3cb0792e3a7f4febbe7b3a34b65741c
ABaudena <alberto.baudena@gmail.com>
2021-10-19 08:39:53 +0200

    added possibility to exit from Scripts/Download_BGC_variables.py without downloading the data

--- Scripts/Download_BGC_variables.py
+++ Scripts/Download_BGC_variables.py
@@ -9,5 +9,7 @@ if __name__ == '__main__':
     from pathlib import Path
     path_to_data = Path('~/GIT/AC_Agulhas_eddy_2021/Data').expanduser()
     os.chdir(str(path_to_data))
+    inp=input('To continue, type any key, otherwise, type "e" to exit:')
+    if inp=='e': print('exited from Download_BGC_variables');exit()
     valid_BGCArgo_id_list=['6903095']
     download_Coriolis_data(valid_BGCArgo_id_list)
