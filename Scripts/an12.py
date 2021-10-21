e38f3517c4518319c666cfa1a748384e9ee45e9b
ABaudena <alberto.baudena@gmail.com>
2021-10-21 12:23:46 +0200

    added data of an12

--- Scripts/an12.py
+++ Scripts/an12.py
@@ -6,9 +6,13 @@ import netCDF4 as nc
 import seawater as sw
 import gsw
 import pandas as pd
-from scipy.signal import savgol_filter
-from scipy.interpolate import griddata
+import pickle
+from paruvpy import mixed_layer_depth
+from CallCppy import Chl_download
+from CallCppy import ellipsoid
 from pathlib import Path
+from scipy.interpolate import griddata
+from matplotlib import path
 home = str(Path.home())
 #globals().clear()
 os.chdir('%s/GIT/AC_Agulhas_eddy_2021/Scripts' % home) #changes directory
@@ -82,9 +86,9 @@ psal_tmp=psal[mask_dens]
 temp_tmp=temp[mask_dens]
 abs_psal_tmp=gsw.SA_from_SP(psal_tmp, pres_tmp, lon_tmp, lat_tmp) # I compute absolute salinity
 cons_tmp=gsw.CT_from_t(abs_psal_tmp, temp_tmp, pres_tmp)          # I compute conservative temperature
-dens_tmp=gsw.density.sigma0(abs_psal_tmp, cons_tmp)
+temp_tmp=gsw.density.sigma0(abs_psal_tmp, cons_tmp)
 dens=np.ones(temp.shape)*99999
-dens[mask_dens]=dens_tmp+1000
+dens[mask_dens]=temp_tmp+1000
 
 #######################################################################
 # I load the eddy center and contours
@@ -97,13 +101,19 @@ data_xVMax=pd.read_csv(filename_xVMax, sep=',', header=None)
 data_yVMax=pd.read_csv(filename_yVMax, sep=',', header=None)
 lonEddy=data_eddy['x_centroid']
 latEddy=data_eddy['y_centroid']
-radius_Vmax=data_eddy['Rmax']
+radius_Vmax=data_eddy['Rmax']   # mean radius_Vmax  = 60 km
+radius_Out=data_eddy['Rout']    # mean radius_Out  = 85 km
+radius_frame=np.mean(radius_Out)*4/111 #radius of the region around the eddy center (in degrees)
 lonVmax=data_xVMax.values[:,:]
 latVmax=data_yVMax.values[:,:]
-Date_Num_Vmax=data_eddy['date_num']
+Date_Num_Eddy=data_eddy['date_num']
+DateTime_Eddy=[]
+i=Date_Num_Eddy[0]
+for i in Date_Num_Eddy:
+    DateTime_Eddy=np.append(DateTime_Eddy,datetime.datetime.fromordinal(int(i)-366))
 
 #######################################################################################################################
-############### Calculating Chlorophyll inside the eddy
+############### Calculating Chlorophyll using BGC Argo data
 #######################################################################################################################
 
 #######################################################################
@@ -113,121 +123,94 @@ Date_Num_Vmax=data_eddy['date_num']
 chla_float_mean=np.squeeze(np.zeros((1,chla.shape[0])))
 chla_float_max=np.squeeze(np.zeros((1,chla.shape[0])))
 
+i=0
 for i in range(0,chla.shape[0]):
     chla_tmp=chla[i,:]
-    sel_chla=chla_tmp!=99999
-    sel_in_ML=depth[i,:]
-    chla_tmp=chla_tmp[sel_chla]
+    depth_tmp=depth[i,:]
+    temp_tmp=temp[i,:]
+    # I exclude nan values
+    sel_non_nan=(chla_tmp!=99999)&(depth_tmp!=99999)&(temp_tmp!=99999)
+    chla_tmp=chla_tmp[sel_non_nan];temp_tmp=temp_tmp[sel_non_nan];depth_tmp=depth_tmp[sel_non_nan]
+    mld,_ = mixed_layer_depth(depth_tmp,temp_tmp,using_temperature='yes')
+    # I select only the chl values inside the mixed layer
+    sel_in_ML=depth_tmp<=mld
+    chla_tmp=chla_tmp[sel_in_ML]
     chla_float_mean[i]=np.mean(chla_tmp)
     chla_float_max[i]=np.max(chla_tmp)
 
+#######################################################################################################################
+############### Calculating Chlorophyll using satellite data
+#######################################################################################################################
+
+delta_chl=0.04 #Resolution of the chlorophyll product, which is used also to inteprolate it
+#### I initialise the chl arrays
+chl_inside_mean=np.squeeze(np.zeros((1,Date_Num_Eddy.size)))
+chl_inside_max=np.squeeze(np.zeros((1,Date_Num_Eddy.size)))
+chl_outside_mean=np.squeeze(np.zeros((1,Date_Num_Eddy.size)))
+chl_inside_and_outside_mean=np.squeeze(np.zeros((1,Date_Num_Eddy.size)))
+
+#### I start the loop on every day for which I have an eddy contour
+i=0
+for i in range(0,Date_Num_Eddy.size):
+    #### I define the frame around the eddy center, for which I want to download the satellite chl data
+    radius_frame_lon=radius_frame / np.cos(lonEddy[i]*np.pi/180)
+    lon0_Chl_Down=lonEddy[i]-radius_frame_lon-0.1
+    lon1_Chl_Down=lonEddy[i]+radius_frame_lon+0.1
+    lat0_Chl_Down=latEddy[i]-radius_frame-0.1
+    lat1_Chl_Down=latEddy[i]+radius_frame+0.1
+    day0 =  np.array((DateTime_Eddy[i].year,DateTime_Eddy[i].month,DateTime_Eddy[i].day))
+    #### I download and load the chlorophyll data
+    chl_filename,chl_name,lonname,latname=Chl_download(day0, lonmin=lon0_Chl_Down, lonmax=lon1_Chl_Down, latmin=lat0_Chl_Down, latmax=lat1_Chl_Down)
+    ds = nc.Dataset(chl_filename)
+    lon_chl = np.squeeze(np.array(ds.variables[lonname]))
+    lat_chl = np.squeeze(np.array(ds.variables[latname]))
+    chl_tmp = np.squeeze(np.array(ds.variables[chl_name]))
+    #### I select the eddy maximal velocity contour for the i-th day (excluding the nan values)
+    lonVmaxtmp = lonVmax[:,i]
+    latVmaxtmp = latVmax[:,i]
+    sel=(~np.isnan(lonVmaxtmp)) & (~np.isnan(latVmaxtmp))
+    lonVmaxtmp = lonVmaxtmp[sel]
+    latVmaxtmp = latVmaxtmp[sel]
+    #plt.plot(lonVmaxtmp,latVmaxtmp,'r.');plt.plot(lonVmaxtmp,latVmaxtmp,'r')
+    # I create a circular cloud frame around the eddy center
+    lons, lats = ellipsoid(lonEddy[i], latEddy[i], radius_frame_lon, radius_frame, delta_chl)
+    #plt.plot(lons,lats,'bo');plt.plot(lonVmaxtmp,latVmaxtmp,'r.');plt.plot(lonVmaxtmp,latVmaxtmp,'r')
+    #### I select only the points of the circular cloud frame which are not inside the eddy maximal velocity contour
+    contour_Vmax=np.concatenate((lonVmaxtmp.reshape(lonVmaxtmp.size,1),latVmaxtmp.reshape(latVmaxtmp.size,1)),axis=1)
+    p = path.Path(contour_Vmax)
+    frame_points = np.concatenate((lons.reshape(lons.size,1),lats.reshape(lats.size,1)),axis=1)
+    sel_outside_contour_Vmax = ~p.contains_points(frame_points)
+    #plt.plot(lons,lats,'bo');plt.plot(lonVmaxtmp,latVmaxtmp,'r.');plt.plot(lonVmaxtmp,latVmaxtmp,'r');plt.plot(lons[sel_outside_contour_Vmax],lats[sel_outside_contour_Vmax],'ko');
+    #### I interpolate the chlorophyll values for each point of the circular cloud frame
+    lon_chl_g, lat_chl_g = np.meshgrid(lon_chl, lat_chl)
+    lon_chl_g, lat_chl_g, chl_tmp = np.squeeze(lon_chl_g.reshape(lon_chl_g.size, 1)), np.squeeze(lat_chl_g.reshape(lat_chl_g.size, 1)), np.squeeze(chl_tmp.reshape(chl_tmp.size, 1))
+    chl_interp = griddata((lon_chl_g, lat_chl_g), chl_tmp, (lons, lats))
+    #### I exclude the nan values (which, in chl_tmp, appear as -999 values)
+    sel = chl_interp>=0
+    chl_interp = chl_interp[sel]
+    sel_outside_contour_Vmax=sel_outside_contour_Vmax[sel]
+    #### I extract the chlorophyll values
+    chl_inside_mean[i] = np.nanmean(chl_interp[~sel_outside_contour_Vmax])
+    chl_inside_max[i] = np.nanmax(chl_interp[~sel_outside_contour_Vmax])
+    chl_outside_mean[i] = np.nanmean(chl_interp[sel_outside_contour_Vmax])
+    chl_inside_and_outside_mean[i] = np.nanmean(chl_interp)
+    #### I remove the chlorophyll file
+    os.system('rm %s' % chl_filename)
+
+
 #######################################################################
-# Calculating Chlorophyll inside the eddy
+# Saving data
 #######################################################################
 
+dictionary_data = {"chla_float_mean": chla_float_mean, "chla_float_max": chla_float_max, "chl_inside_mean": chl_inside_mean,
+                   "chl_inside_max": chl_inside_max, "chl_outside_mean": chl_outside_mean, "chl_inside_and_outside_mean": chl_inside_and_outside_mean,
+                   "Date_Num_Eddy": Date_Num_Eddy, "lonEddy": lonEddy, "latEddy": latEddy,
+                   "lon_float": lon, "lat_float": lat, "Date_Num_float": Date_Num, "Date_Vec_float": Date_Vec}
+a_file = open("%s/an12/data_an12.pkl" % storedir, "wb")
+pickle.dump(dictionary_data, a_file)
+a_file.close()
 
 
 
 
 
-
-parameter=temp
-parameter_ylabel_list=['Temperature ($^{\circ}$C)','Pratical salinity (psu)','Chlorophyll-a (mg/m$^3$)','Dissolved oxygen ($\mu$mol/kg)','Particle backscattering (10$^5$ m$^{-1}$)']
-parameter_shortname_list=['temp','psal','chla','doxy','bbp700']
-ipar=3
-for ipar in range(0,parameter_ylabel_list.__len__()):
-    if ipar==0: parameter=temp
-    elif ipar==1:   parameter=psal
-    elif ipar == 2: parameter=chla
-    elif ipar == 3: parameter=doxy
-    elif ipar == 4: parameter=bbp700
-
-    #I filter the profiles
-    parameter_filtered=np.array([]);Date_Num_parameter=np.array([]);depth_parameter=np.array([]);dens_parameter=np.array([]);pressure_parameter=np.array([])
-    i=0
-    for i in range(0,parameter.shape[0]):
-        sel=(parameter[i,:]!=99999) & (depth[i,:]!=99999) & (dens[i,:]!=99999)
-        if sum(sel) > 0:
-            z=parameter[i,sel];x=np.ones(z.shape)*Date_Num[i];y1=depth[i,sel];y2=dens[i,sel];y3=pres[i,sel]
-            z = savgol_filter(z, 5, 1)
-            parameter_filtered = np.concatenate((parameter_filtered, z));Date_Num_parameter = np.concatenate((Date_Num_parameter, x))
-            depth_parameter = np.concatenate((depth_parameter, y1));dens_parameter = np.concatenate((dens_parameter, y2));pressure_parameter = np.concatenate((pressure_parameter, y3))
-
-    parameter_filtered[parameter_filtered<0]=0
-    # I define the x and y arrays for the contourf plot
-    x_parameter = np.linspace(Date_Num_parameter.min(),Date_Num_parameter.max(),100)
-    y1_parameter = np.linspace(depth_parameter.min(),depth_parameter.max(),50)
-    y2_parameter = np.linspace(dens_parameter.min(),dens_parameter.max(),50)
-    y3_parameter = np.linspace(pressure_parameter.min(),pressure_parameter.max(),50)
-    # I interpolate
-    x_parameter_g,y_parameter_g=np.meshgrid(x_parameter,y1_parameter)
-    parameter_interp_depth = griddata((Date_Num_parameter,depth_parameter), parameter_filtered, (x_parameter_g, y_parameter_g), method="nearest")
-    x_parameter_g,y_parameter_g=np.meshgrid(x_parameter,y2_parameter)
-    parameter_interp_dens = griddata((Date_Num_parameter,dens_parameter), parameter_filtered, (x_parameter_g, y_parameter_g), method="nearest")
-
-
-    ########################################################
-    ####### I plot: versus depth
-    ########################################################
-    if ipar==4:
-        parameter_interp_depth[parameter_interp_depth > 2*10**(-3)] = 2*10**(-3)
-
-    width, height = 0.8, 0.7
-    set_ylim_lower, set_ylim_upper = y1_parameter.min(),600
-    fig = plt.figure(1, figsize=(12,8))
-    ax = fig.add_axes([0.12, 0.2, width, height], ylim=(set_ylim_lower, set_ylim_upper), xlim=(Date_Num.min(), Date_Num.max()))
-    ax_1 = plot2 = plt.contourf(x_parameter,y1_parameter, parameter_interp_depth)
-    plt.gca().invert_yaxis()
-    # draw colorbar
-    cbar = plt.colorbar(plot2)
-    cbar.ax.set_ylabel(parameter_ylabel_list[ipar], fontsize=18)
-    plt.ylabel('Depth (m)', fontsize=18)
-    #plt.title('%smm' % NP_sizeclass, fontsize=18)
-    #I set xticks
-    nxticks=10
-    xticks=np.linspace(Date_Num.min(),Date_Num.max(),nxticks)
-    xticklabels=[]
-    for i in xticks:
-        date_time_obj = date_reference + datetime.timedelta(days=i)
-        xticklabels.append(date_time_obj.strftime('%d %B'))
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(xticklabels)
-    plt.xticks(rotation=90,fontsize=12)
-    # I add the grid
-    plt.grid(color='k', linestyle='dashed', linewidth=0.5)
-    plt.savefig('../Plots/an05/TimeSeries_%s_vs_01depth_an05.pdf' % parameter_shortname_list[ipar],dpi=200)
-    plt.close()
-
-    ########################################################
-    ####### I plot: versus density
-    ########################################################
-    if ipar==3:
-        parameter_interp_dens[parameter_interp_dens > 255] = 255
-    if ipar==4:
-        parameter_interp_dens[parameter_interp_dens > 2*10**(-3)] = 2*10**(-3)
-
-    width, height = 0.8, 0.7
-    set_ylim_lower, set_ylim_upper = y2_parameter.min(), y2_parameter.max()
-    fig = plt.figure(1, figsize=(12,8))
-    ax = fig.add_axes([0.12, 0.2, width, height], ylim=(set_ylim_lower, set_ylim_upper), xlim=(Date_Num.min(), Date_Num.max()))
-    ax_1 = plot2 = plt.contourf(x_parameter,y2_parameter, parameter_interp_dens)
-    plt.gca().invert_yaxis()
-    # draw colorbar
-    cbar = plt.colorbar(plot2)
-    cbar.ax.set_ylabel(parameter_ylabel_list[ipar], fontsize=18)
-    plt.ylabel('Density (kg/m$^3$)', fontsize=18)
-    #plt.title('%smm' % NP_sizeclass, fontsize=18)
-    #I set xticks
-    nxticks=10
-    xticks=np.linspace(Date_Num.min(),Date_Num.max(),nxticks)
-    xticklabels=[]
-    for i in xticks:
-        date_time_obj = date_reference + datetime.timedelta(days=i)
-        xticklabels.append(date_time_obj.strftime('%d %B'))
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(xticklabels)
-    plt.xticks(rotation=90,fontsize=12)
-    # I add the grid
-    plt.grid(color='k', linestyle='dashed', linewidth=0.5)
-    plt.savefig('../Plots/an05/TimeSeries_%s_vs_02dens_an05.pdf' % parameter_shortname_list[ipar],dpi=200)
-    plt.close()
