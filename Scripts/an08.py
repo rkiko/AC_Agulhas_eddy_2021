24cafbf5788168660434882dd01fc2bfe9ea05dc
ABaudena <alberto.baudena@gmail.com>
2021-10-22 18:08:02 +0200

    added integrated POC time series calculation and plot

--- Scripts/an08.py
+++ Scripts/an08.py
@@ -35,6 +35,8 @@ MiP_abund=np.array(data['MiP_abun'][sel_filename])
 MaP_abund=np.array(data['MaP_abun'][sel_filename])
 MiP_POC=np.array(data['Mip_POC_cont_mgC_m3'][sel_filename])
 MaP_POC=np.array(data['Map_POC_cont_mgC_m3'][sel_filename])
+bbp_POC=np.array(data['bbp POC [mgC/m3]'][sel_filename])
+depth=np.array(data['Depth [m]'][sel_filename])
 
 # I convert the dates to float values (in seconds from 1970 1 1)
 Date_Num=np.r_[0:Flux.size]
@@ -42,14 +44,6 @@ for i in Date_Num:
     date_time_obj = datetime.strptime(Date_Time[i], '%Y-%m-%dT%H:%M:%S')
     Date_Num[i] = calendar.timegm(date_time_obj.timetuple())
     #datetime.utcfromtimestamp(Date_Num[i])
-
-#I tranform the pressure to depth
-mask_depth=pressure!=99999 #I select only valid values
-lat_tmp=lat[mask_depth]
-pressure_tmp=pressure[mask_depth]
-depth_tmp=sw.eos80.dpth(pressure_tmp, lat_tmp)
-depth=np.ones(pressure.shape)*99999
-depth[mask_depth]=depth_tmp
 
 list_dates=np.unique(Date_Num)
 #I start the loop on the different parameters I plot
@@ -59,12 +53,16 @@ parameter_shortname_list=['Flux','MiP_abund','MaP_abund','MiP_POC','MaP_POC']
 parameter_ylabel_list=['Flux (mgC $m^{-2}$ $d^{-1}$)','MiP abundance (# L$^{-1}$)','MaP abundance (# L$^{-1}$)'
     ,'MiP (mgC $m^{-3}$)','MaP (mgC $m^{-3}$)']
 max_parameter_list=np.array([6,65,0.6,1.15,0.012])
-for ipar in range(0,parameter_ylabel_list.__len__()):
+MiP_POC_0_200=np.array([]);MiP_POC_200_600=np.array([])
+MaP_POC_0_200=np.array([]);MaP_POC_200_600=np.array([])
+bbp_POC_0_200=np.array([]);bbp_POC_200_600=np.array([])
+for ipar in range(0,parameter_ylabel_list.__len__()+1):
     if ipar==0: parameter=Flux.copy()
     elif ipar==1:   parameter=MiP_abund.copy()
     elif ipar == 2: parameter=MaP_abund.copy()
     elif ipar == 3: parameter=MiP_POC.copy()
     elif ipar == 4: parameter=MaP_POC.copy()
+    elif ipar == 5: parameter=bbp_POC.copy()
 
     # I filter the flux prophiles
     parameter_filtered=np.array([]);depth_filtered=np.array([]);Date_Num_filtered=np.array([])
@@ -77,7 +75,14 @@ for ipar in range(0,parameter_ylabel_list.__len__()):
             parameter_filtered = np.concatenate((parameter_filtered, z))
             Date_Num_filtered = np.concatenate((Date_Num_filtered, x2))
             depth_filtered = np.concatenate((depth_filtered, y2))
+            # sel_200 and sel_200_600 are used only for the POC integrated in time
+            sel_0_200 = np.abs(y2) < 200
+            sel_200_600 = (np.abs(y2) >= 200) & (np.abs(y2) <600)
+            if ipar==3: MiP_POC_0_200=np.append(MiP_POC_0_200,np.mean(z[sel_0_200]));MiP_POC_200_600=np.append(MiP_POC_200_600,np.mean(z[sel_200_600]))
+            if ipar==4: MaP_POC_0_200=np.append(MaP_POC_0_200,np.mean(z[sel_0_200]));MaP_POC_200_600=np.append(MaP_POC_200_600,np.mean(z[sel_200_600]))
+            if ipar==5: bbp_POC_0_200=np.append(bbp_POC_0_200,np.mean(z[sel_0_200]));bbp_POC_200_600=np.append(bbp_POC_200_600,np.mean(z[sel_200_600]))
 
+    if ipar==5: continue # I do not plot the bbp as it is already plotted in an05 (with higher resolution data)
     # I define the x and y arrays for the contourf plot
     x_filtered = np.linspace(Date_Num_filtered.min(),Date_Num_filtered.max(),100)
     y_filtered = np.linspace(depth_filtered.min(),depth_filtered.max(),100)
@@ -162,4 +167,96 @@ for ipar in range(0,parameter_ylabel_list.__len__()):
     plt.savefig('../Plots/an08/TimeSeries%02dB%s_an08.pdf' % (ipar,parameter_shortname_list[ipar]),dpi=200)
     plt.close()
 
+
+
+###########################################################################
+###########################################################################
+# I plot the integrated POC budget over time, for the layer 0-200m and 200-600m
+###########################################################################
+###########################################################################
+
+POC_0_200=MiP_POC_0_200+MaP_POC_0_200+bbp_POC_0_200
+POC_200_600=MiP_POC_200_600+MaP_POC_200_600+bbp_POC_200_600
+POC_0_200[POC_0_200<0]=0
+POC_200_600[POC_200_600<0]=0
+# Parameters for the plot
+width, height = 0.8, 0.7
+set_ylim_lower, set_ylim_upper = min(POC_0_200.min(),POC_200_600.min()),max(POC_0_200.max(),POC_200_600.max())
+
+#POC 0-200 vs 200-600
+fig = plt.figure(1, figsize=(12,8))
+ax = fig.add_axes([0.12, 0.2, width, height], ylim=(set_ylim_lower, set_ylim_upper*1.1), xlim=(list_dates.min(), list_dates.max()))
+plt.plot(list_dates,POC_0_200,'r',label='0-200 m')
+plt.scatter(list_dates,POC_0_200,c='r')
+plt.plot(list_dates,POC_200_600,'b',label='200-600 m')
+plt.scatter(list_dates,POC_200_600,c='b')
+# I set xticks
+nxticks = 10
+xticks = np.linspace(list_dates.min(), list_dates.max(), nxticks)
+xticklabels = []
+for i in xticks:
+    xticklabels.append(datetime.utcfromtimestamp(i).strftime('%d %B'))
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticklabels)
+plt.xticks(rotation=90, fontsize=14)
+plt.legend(fontsize=14)
+plt.ylabel('Integrated POC (mgC/m$^3$)', fontsize=18)
+plt.grid(color='k', linestyle='dashed', linewidth=0.5)
+plt.savefig('../Plots/an08/IntegratedPOC_vs_time_an08.pdf' ,dpi=200)
+plt.close()
+
+
+
+# 0-200 m layer
+fig = plt.figure(1, figsize=(12,8))
+ax = fig.add_axes([0.12, 0.2, width, height], ylim=(set_ylim_lower, set_ylim_upper*1.1), xlim=(list_dates.min(), list_dates.max()))
+plt.plot(list_dates,bbp_POC_0_200,'y',label='bbpPOC')
+plt.scatter(list_dates,bbp_POC_0_200,c='y')
+plt.plot(list_dates,MiP_POC_0_200,'c',label='MiP')
+plt.scatter(list_dates,MiP_POC_0_200,c='c')
+plt.plot(list_dates,MaP_POC_0_200,'g',label='MaP')
+plt.scatter(list_dates,MaP_POC_0_200,c='g')
+# I set xticks
+nxticks = 10
+xticks = np.linspace(list_dates.min(), list_dates.max(), nxticks)
+xticklabels = []
+for i in xticks:
+    xticklabels.append(datetime.utcfromtimestamp(i).strftime('%d %B'))
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticklabels)
+plt.xticks(rotation=90, fontsize=14)
+plt.legend(fontsize=14)
+plt.ylabel('Integrated POC (mgC/m$^3$)', fontsize=18)
+plt.title('0-200 m layer', fontsize=18)
+plt.grid(color='k', linestyle='dashed', linewidth=0.5)
+plt.savefig('../Plots/an08/0_200m_MiP_MaP_bbpPOC_vs_time_an08.pdf' ,dpi=200)
+plt.close()
+
+
+
+
+# 200-600 m layer
+fig = plt.figure(1, figsize=(12,8))
+ax = fig.add_axes([0.12, 0.2, width, height], ylim=(set_ylim_lower, set_ylim_upper*1.1), xlim=(list_dates.min(), list_dates.max()))
+plt.plot(list_dates,bbp_POC_200_600,'y',label='bbpPOC')
+plt.scatter(list_dates,bbp_POC_200_600,c='y')
+plt.plot(list_dates,MiP_POC_200_600,'c',label='MiP')
+plt.scatter(list_dates,MiP_POC_200_600,c='c')
+plt.plot(list_dates,MaP_POC_200_600,'g',label='MaP')
+plt.scatter(list_dates,MaP_POC_200_600,c='g')
+# I set xticks
+nxticks = 10
+xticks = np.linspace(list_dates.min(), list_dates.max(), nxticks)
+xticklabels = []
+for i in xticks:
+    xticklabels.append(datetime.utcfromtimestamp(i).strftime('%d %B'))
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticklabels)
+plt.xticks(rotation=90, fontsize=14)
+plt.legend(fontsize=14)
+plt.ylabel('Integrated POC (mgC/m$^3$)', fontsize=18)
+plt.title('200-600 m layer', fontsize=18)
+plt.grid(color='k', linestyle='dashed', linewidth=0.5)
+plt.savefig('../Plots/an08/200_600m_MiP_MaP_bbpPOC_vs_time_an08.pdf' ,dpi=200)
+plt.close()
 
