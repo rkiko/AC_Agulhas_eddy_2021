ef70ceac92e9e08b0e969cefdf27af909dbf3f3b
ABaudena <alberto.baudena@gmail.com>
2022-02-05 15:55:12 +0100

    calculated calculate_diffPSD_slope_func and appended to Ecopart_processed_data.tsv

--- Scripts/Append_BGC_variables.py
+++ Scripts/Append_BGC_variables.py
@@ -4,6 +4,7 @@ Include the BGC variables downloaded from the Coriolis data center via ftp
 
 import pandas as pd
 from paruvpy import include_Coriolis_data
+from paruvpy import calculate_diffPSD_slope_func
 
 
 if __name__ == '__main__':
@@ -13,6 +14,7 @@ if __name__ == '__main__':
     os.chdir(str(path_to_data))
     df = pd.read_csv("Ecopart_processed_data.tsv", sep='\t')
     df = include_Coriolis_data(df)
+    df = calculate_diffPSD_slope_func(df)
     df.to_csv('Ecopart_processed_data.tsv', sep='\t', index=False)
 
 
