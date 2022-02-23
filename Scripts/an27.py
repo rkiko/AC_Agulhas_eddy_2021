4a13e846ca7e600d80bb8d7e974be9b8936fe9d9
ABaudena <alberto.baudena@gmail.com>
2022-02-23 10:00:30 +0100

    used depth instead of pressure, removed outlier

--- Scripts/an27.py
+++ Scripts/an27.py
@@ -34,10 +34,11 @@ for a in RAWfilename:
 
 # I extract the data
 lon=np.array(data.Longitude[sel_filename]);lat=np.array(data.Latitude[sel_filename])
-Date_Time=np.array(data.Date_Time[sel_filename]);pressure=-np.array(data.Pressure_dbar[sel_filename])
+Date_Time=np.array(data.Date_Time[sel_filename]);
+depth=np.array(data['Depth_m'][sel_filename])
 
 # I convert the dates to float values (in seconds from 1970 1 1)
-Date_Num=np.r_[0:pressure.size]
+Date_Num=np.r_[0:depth.size]
 for i in Date_Num:
     date_time_obj = datetime.strptime(Date_Time[i], '%Y-%m-%dT%H:%M:%S')
     Date_Num[i] = calendar.timegm(date_time_obj.timetuple())
@@ -65,39 +66,39 @@ for iNP in range(27,32):
     threshold_value=threshold_value_list[0]
     for threshold_value in threshold_value_list:
         # I filter the prophiles
-        NP_filtered=np.array([]);pressure_NP=np.array([]);Date_Num_NP=np.array([])
+        NP_filtered=np.array([]);depth_NP=np.array([]);Date_Num_NP=np.array([])
         list_dates=np.unique(Date_Num)
-        pressure_threshold=np.array([]);Date_Num_threshold=np.array([])
+        depth_threshold=np.array([]);Date_Num_threshold=np.array([])
         for i in range(0,list_dates.size):
-            sel=Date_Num==list_dates[i];x=Date_Num[sel];y=pressure[sel]
+            sel=Date_Num==list_dates[i];x=Date_Num[sel];y=depth[sel]
             z=NP_abun[sel];sel2=~np.isnan(z);z=z[sel2];x2=x[sel2];y2=y[sel2]
             if sum(sel2)>0:
                 z=savgol_filter(z,5,1)
-                NP_filtered = np.concatenate((NP_filtered, z));Date_Num_NP = np.concatenate((Date_Num_NP, x2));pressure_NP = np.concatenate((pressure_NP, y2))
+                NP_filtered = np.concatenate((NP_filtered, z));Date_Num_NP = np.concatenate((Date_Num_NP, x2));depth_NP = np.concatenate((depth_NP, y2))
                 ct = 0
                 for iz in range(0,z.size-1):
-                    if np.logical_and(Date_Num_limit[0]<=list_dates[i]<=Date_Num_limit[1],z[iz+1]<threshold_value<z[iz]):
+                    if (Date_Num_limit[0]<=list_dates[i]<=Date_Num_limit[1])&(z[iz+1]<threshold_value<z[iz])&(list_dates[i]!=1629078553): #I exclude an outlier
                         ct=ct+1
                         dist=(threshold_value-z[iz])/(z[iz+1]-z[iz])
-                        pressure_tmp=y[iz]-abs((y[iz+1]-y[iz])*dist)
-                        #pressure_threshold = np.append(pressure_threshold, pressure_tmp)
+                        depth_tmp=y[iz]-abs((y[iz+1]-y[iz])*dist)
+                        #depth_threshold = np.append(depth_threshold, depth_tmp)
                         #Date_Num_threshold = np.append(Date_Num_threshold, list_dates[i])
                 if ct==1:
-                    pressure_threshold = np.append(pressure_threshold, pressure_tmp)
+                    depth_threshold = np.append(depth_threshold, depth_tmp)
                     Date_Num_threshold = np.append(Date_Num_threshold, list_dates[i])
 
-        (interpol,_,_,signif,signif_label)=lin_fit(Date_Num_threshold/86400,pressure_threshold)
+        (interpol,_,_,signif,signif_label)=lin_fit(Date_Num_threshold/86400,depth_threshold)
         settling_vel=np.append(settling_vel,interpol.slope)
         print('%s mm, Threshold %0.1f #/L, settling velocity: %0.2f m/d' % (NP_sizeclass,threshold_value,interpol.slope))
         # I define the x and y arrays for the contourf plot
         x_NP = np.linspace(Date_Num_NP.min(), Date_Num_NP.max(), 100)
-        y_NP = np.linspace(pressure_NP.min(), pressure_NP.max(), 50)
+        y_NP = np.linspace(depth_NP.min(), depth_NP.max(), 50)
         x_NP_g, y_NP_g = np.meshgrid(x_NP, y_NP)
         # I interpolate
-        NP_interp = griddata((Date_Num_NP, pressure_NP), NP_filtered, (x_NP_g, y_NP_g), method="nearest")
+        NP_interp = griddata((Date_Num_NP, depth_NP), NP_filtered, (x_NP_g, y_NP_g), method="nearest")
 
         width, height = 0.8, 0.7
-        set_ylim_lower, set_ylim_upper = -600, pressure_NP.max()
+        set_ylim_lower, set_ylim_upper = -600, depth_NP.max()
         fig = plt.figure(1, figsize=(12, 8))
         ax = fig.add_axes([0.12, 0.2, width, height], ylim=(set_ylim_lower, set_ylim_upper),
                           xlim=(Date_Num.min(), Date_Num.max()))
@@ -106,10 +107,10 @@ for iNP in range(27,32):
         ax_1 = plot2 = plt.contourf(x_NP, y_NP, NP_plot)  # , vmax=np.log(maxNP))#, cmap=cmhot)
         # draw colorbar
         cbar = plt.colorbar(plot2)
-        plot3 = plt.scatter(Date_Num_threshold, pressure_threshold, c='black')
+        plot3 = plt.scatter(Date_Num_threshold, depth_threshold, c='black')
         plot4= plt.plot(np.linspace(Date_Num_limit[0],Date_Num_limit[1],20),np.linspace(Date_Num_limit[0]*interpol.slope/86400+interpol.intercept,Date_Num_limit[1]*interpol.slope/86400+interpol.intercept,20),c='black')
         cbar.ax.set_ylabel('NP abundance (#/L)', fontsize=18)
-        plt.ylabel('Pressure (dbar)', fontsize=18)
+        plt.ylabel('Depth (m)', fontsize=18)
         plt.title('%smm, thr: %0.1f #/L, Settling vel: %0.2f m/d, fit signif: %s' % (NP_sizeclass,threshold_value,interpol.slope,signif_label), fontsize=18)
         nxticks = 10
         xticks = np.linspace(Date_Num.min(), Date_Num.max(), nxticks)
@@ -143,13 +144,14 @@ ax = fig.add_axes([0.15, 0.15, width, height])
 plt.plot(sizeclass_list,np.abs(settling_vel_mean),'o')
 plt.plot(sizeclass_list,np.abs(settling_vel_mean),label='This paper')
 plt.errorbar(sizeclass_list,np.abs(settling_vel_mean),yerr=settling_vel_std,capsize=5)
-plt.plot(sizeclass_list,np.abs(sink_k2002),label='xxx et al.')
 plt.grid(color='k', linestyle='dashed', linewidth=0.3)
 plt.xlabel('Size class (mm)', fontsize=10)
 plt.ylabel('Settling velocity (m/d)', fontsize=10)
 plt.title('Relationship between size class\nand settling velocity', fontsize=10)
 plt.xticks(fontsize=10),plt.yticks(fontsize=10)
+plt.savefig('../Plots/an27/relationship_size_vs_settlingVelocity_alone_an27.pdf', dpi=200)
+plt.plot(sizeclass_list,np.abs(sink_k2002),label='xxx et al.')
 plt.legend(fontsize=7)
-plt.savefig('../Plots/an27/relationship_size_vs_settlingVelocity_an27.pdf', dpi=200)
+plt.savefig('../Plots/an27/relationship_size_vs_settlingVelocity_comparison_an27.pdf', dpi=200)
 plt.close()
 
