c1627f1af71553ee1350040ad6422816661e99c1
ABaudena <alberto.baudena@gmail.com>
2022-02-28 16:15:31 +0100

    changed db with ecopart_function, and changed the script so that it asks for password and username, and takes as argument the number of the project I want to download, as in Lagrangian_uvp/Download_ecopart

--- Scripts/Download_particle_histogram.py
+++ Scripts/Download_particle_histogram.py
@@ -1,4 +1,4 @@
-from paruvpy import db_connect
+from paruvpy import ecopart_connect
 from paruvpy import read_particle_histogram_data_floats
 import sys
 
@@ -10,11 +10,14 @@ if __name__ == '__main__':
     path_to_data = Path('~/GIT/AC_Agulhas_eddy_2021/Data').expanduser()
     os.chdir(str(path_to_data))
 
+    id_project = int(sys.argv[1])
+    filename = sys.argv[2]
+
     user_ = getpass.getpass('Username (note, if you want to exit the script Download_particle_histogram, please write "e" as input:')
-    if user_=='e':  print('exited from Download_particle_histogram');exit()
+    if user_=='e':  print('exited from Download_particle_histogram_%d' % id_project);exit()
     passwd = getpass.getpass('Password (note, if you want to exit the script Download_particle_histogram, please write "e" as input::')
-    if passwd == 'e':  print('exited from Download_particle_histogram');exit()
-    mydb = db_connect(user_, passwd)
-    valid_projectid_list = '(356)'
+    if passwd == 'e':  print('exited from Download_particle_histogram_%d' % id_project);exit()
+    mydb = ecopart_connect(user_, passwd)
+    valid_projectid_list = '(%d)' % id_project
     df = read_particle_histogram_data_floats(valid_projectid_list, mydb)
-    df.to_csv('Ecopart_histogram_data_raw.tsv', sep='\t', index=False)
+    df.to_csv('%s' % filename,sep='\t', index=False)
