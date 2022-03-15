cf0529a20625f68eeb190e2edaf7ef71476d5b87
ABaudena <alberto.baudena@gmail.com>
2022-03-15 11:34:10 +0100

    after downloading the data, I keep only those in our study region

--- Scripts/an35.py
+++ Scripts/an35.py
@@ -20,5 +20,36 @@ for index_day in range(day0,dayf+1):
     os.system("python obdaac_download.py -v %s" % (url_filename_download))
     os.system("mv %s ../Data/an35/" % (filename))
 
+import netCDF4 as nc
+import numpy as np
+lonmin,lonmax = 5,18
+latmin,latmax = -40, -30
 
-
+index_day=day0
+for index_day in range(day0,dayf+1):
+    filename = '%s%d%03d.%s' % (filename0,year,index_day,filename2)
+    filenameB = '%s%d%03d.%sB.nc' % (filename0,year,index_day,filename2[:-3])
+    ds = nc.Dataset("../Data/an35/%s" % (filename))
+    lon2 = np.array(ds.variables['lon'])
+    lat2 = np.array(ds.variables['lat'])
+    zeu2 = np.array(ds.variables['Zeu_lee'])
+    ilonmin=np.where(np.abs(lon2-lonmin) == (np.abs(lon2-lonmin)).min())[0][0]
+    ilonmax=np.where(np.abs(lon2-lonmax) == (np.abs(lon2-lonmax)).min())[0][0]
+    ilatmin=np.where(np.abs(lat2-latmin) == (np.abs(lat2-latmin)).min())[0][0]
+    ilatmax=np.where(np.abs(lat2-latmax) == (np.abs(lat2-latmax)).min())[0][0]
+    lon2 = lon2[ilonmin:ilonmax]
+    lat2 = lat2[ilatmax:ilatmin]
+    zeu2 = zeu2[ilatmax:ilatmin,ilonmin:ilonmax]
+    ds.close()
+    ds = nc.Dataset("../Data/an35/%s" % (filenameB),mode='w')
+    ds.createDimension('lat', lat2.size)
+    ds.createDimension('lon', lon2.size)
+    lat = ds.createVariable('lat', np.float32, ('lat',))
+    lon = ds.createVariable('lon', np.float32, ('lon',))
+    zeu = ds.createVariable('Zeu_lee', np.float32, ('lat','lon'))
+    lon[:] = lon2
+    lat[:] = lat2
+    zeu[:,:] = zeu2
+    ds.close()
+    os.system("rm -f ../Data/an35/%s" % (filename))
+    os.system("mv ../Data/an35/%s ../Data/an35/%s" % (filenameB,filename))
