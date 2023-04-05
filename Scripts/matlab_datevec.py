ea2d6ade574c4bdda58648c00389a306e14522ce
ABaudena <alberto.baudena@gmail.com>
2023-04-05 18:54:20 +0200

    improved approximation of date

--- Scripts/matlab_datevec.py
+++ Scripts/matlab_datevec.py
@@ -5,6 +5,7 @@ from datetime import date
 def matlab_datevec(x):
 
     fracx=x-np.floor(x)
+    fracx = np.round(fracx * 86400) / 86400 # I round the seconds (i.e. if a date is e.g. 2h 34' 56.9" I approximate it at 2h 34' 57"
     y = date.fromordinal(int(np.floor(x)-366))
     hour=np.floor(fracx*24)
     hourfrac=fracx*24-hour
